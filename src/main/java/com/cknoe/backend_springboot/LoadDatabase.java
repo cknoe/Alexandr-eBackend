bbdf81958bde019792d0f8d4dcf5aa105443222e
ck <none>
2025-09-02 22:20:50 +0200

    profil spring

--- src/main/java/com/cknoe/backend_springboot/LoadDatabase.java
+++ src/main/java/com/cknoe/backend_springboot/LoadDatabase.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("test")
 public class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
@@ -14,7 +16,7 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(CardRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Card("Title 1", "Description 1", "Content 1")));
+            log.info("Preloading " + repository.save(new Card("H2 card", "test profile", "Content 1")));
             log.info("Preloading " + repository.save(new Card("Title 2", "Description 2", "Content 2")));
         };
     }
