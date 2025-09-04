7e7782d77dc1ba005e075a117250d5c9092fc960
ck <none>
2025-09-05 00:57:34 +0200

    restricting access to card data

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ docker-compose up -d
 #Connection
 curl -v -X POST http://localhost:8080/login -H "Content-Type: application/json" -d '{"username":"testuser","password":"password"}' | jq
 curl -v http://localhost:8080/cards -H "Authorization: Bearer TOKEN"| jq
+
+
+quand je mettrai de nouveaux role, il faudra changer cardcontroller
--- src/main/java/com/cknoe/backend_springboot/Controller/CardController.java
+++ src/main/java/com/cknoe/backend_springboot/Controller/CardController.java
@@ -17,6 +17,7 @@ import com.cknoe.backend_springboot.dto.CardDTO;
 import com.cknoe.backend_springboot.entity.AppUser;
 import com.cknoe.backend_springboot.entity.Card;
 import com.cknoe.backend_springboot.exception.CardNotFoundException;
+import com.cknoe.backend_springboot.exception.ForbiddenException;
 import com.cknoe.backend_springboot.repository.CardRepository;
 import com.cknoe.backend_springboot.repository.AppUserRepository;
 
@@ -31,39 +32,70 @@ public class CardController {
         this.appUserRepository = appUserRepository;
     }
 
-    @GetMapping("/admincards")
-    List<CardDTO> all() {
-        return cardRepository.findAll().stream().map(CardDTO::fromEntity).toList();
-    }
-
     @GetMapping("/cards")
-    List<CardDTO> myCards(@AuthenticationPrincipal UserDetails userDetails) {
+    List<CardDTO> getMyCards(@AuthenticationPrincipal UserDetails userDetails) {
         String username = userDetails.getUsername();
         return cardRepository.findByOwnerUsername(username).stream().map(CardDTO::fromEntity).toList();
     }
 
-    @PostMapping("/admincards")
-    Card newCard(@RequestBody Card newCard) {
-        return cardRepository.save(newCard);
-    }
-
     @PostMapping("/cards")
-    public ResponseEntity<Card> createCard(@RequestBody Card card, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<CardDTO> createCard(@RequestBody Card card,
+            @AuthenticationPrincipal UserDetails userDetails) {
         AppUser user = appUserRepository.findByUsername(userDetails.getUsername())
                 .orElseThrow(() -> new RuntimeException("User not found"));
         card.setOwner(user);
-        return ResponseEntity.ok(cardRepository.save(card));
+        return ResponseEntity.ok(CardDTO.fromEntity(cardRepository.save(card)));
     }
 
     @GetMapping("/cards/{id}")
-    CardDTO one(@PathVariable Long id) {
+    CardDTO getMyCard(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
         Card card = cardRepository.findById(id).orElseThrow(() -> new CardNotFoundException(id));
+        if (card.getOwner().getUsername() != userDetails.getUsername()) {
+            throw new ForbiddenException();
+        }
         return CardDTO.fromEntity(card);
     }
 
     @PutMapping("/cards/{id}")
-    Card replaceCard(@RequestBody Card newCard, @PathVariable Long id) {
-        return cardRepository.findById(id)
+    CardDTO replaceMyCard(@RequestBody Card newCard, @PathVariable Long id,
+            @AuthenticationPrincipal UserDetails userDetails) {
+        Card updatedCard = cardRepository.findById(id)
+                .map(card -> {
+                    if (!card.getOwner().getUsername().equals(userDetails.getUsername())) {
+                        throw new ForbiddenException();
+                    }
+                    card.setTitle(newCard.getTitle());
+                    card.setDescription(newCard.getDescription());
+                    card.setContent(newCard.getContent());
+                    return cardRepository.save(card);
+                })
+                .orElseThrow(() -> new CardNotFoundException(id));
+        ;
+        return CardDTO.fromEntity(updatedCard);
+    }
+
+    @DeleteMapping("/cards/{id}")
+    void deleteMyCard(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
+        Card card = cardRepository.findById(id).orElseThrow(() -> new CardNotFoundException(id));
+        if (!card.getOwner().getUsername().equals(userDetails.getUsername())) {
+            throw new ForbiddenException();
+        }
+        cardRepository.deleteById(id);
+    }
+
+    @GetMapping("/admin/cards")
+    List<CardDTO> all() {
+        return cardRepository.findAll().stream().map(CardDTO::fromEntity).toList();
+    }
+
+    @PostMapping("/admin/cards")
+    Card newCard(@RequestBody Card newCard) {
+        return cardRepository.save(newCard);
+    }
+
+    @PutMapping("/admin/cards/{id}")
+    CardDTO replaceCard(@RequestBody Card newCard, @PathVariable Long id) {
+        Card updatedCard = cardRepository.findById(id)
                 .map(card -> {
                     card.setTitle(newCard.getTitle());
                     card.setDescription(newCard.getDescription());
@@ -74,11 +106,6 @@ public class CardController {
                     newCard.setId(id);
                     return cardRepository.save(newCard);
                 });
+        return CardDTO.fromEntity(updatedCard);
     }
-
-    @DeleteMapping("/cards/{id}")
-    void deleteCard(@PathVariable Long id) {
-        cardRepository.deleteById(id);
-    }
-
 }
