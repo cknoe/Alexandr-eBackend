ce9d2f391b1774f900d63bb772f76c944460b7f9
ck <none>
2025-09-15 04:54:17 +0200

    username constraints

--- src/main/java/com/cknoe/backend_springboot/dto/RegisterRequestDTO.java
+++ src/main/java/com/cknoe/backend_springboot/dto/RegisterRequestDTO.java
@@ -4,9 +4,10 @@ import com.cknoe.backend_springboot.entity.AppUser;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record RegisterRequestDTO(
-        @NotBlank String username,
+        @NotBlank @Pattern(regexp = "^[A-Za-z0-9_]{1,30}$", message = "Username must be 1 to 30 characters : letters, numbers or underscore only") String username,
         @NotNull String password) {
 
     public static AppUser toEntity(RegisterRequestDTO registerRequestDTO) {
