c4265854c0f5e2e9b7705ea0aa0cca3c96b0524a
ck <none>
2025-09-18 02:39:33 +0200

    Owner in cardCollectionDTO

--- src/main/java/com/cknoe/backend_springboot/dto/CardCollectionDTO.java
+++ src/main/java/com/cknoe/backend_springboot/dto/CardCollectionDTO.java
@@ -3,6 +3,7 @@ package com.cknoe.backend_springboot.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.cknoe.backend_springboot.dto.userDTOs.UserDTO;
 import com.cknoe.backend_springboot.entity.CardCollection;
 
 import jakarta.validation.constraints.NotBlank;
@@ -10,14 +11,16 @@ import jakarta.validation.constraints.NotBlank;
 public record CardCollectionDTO(
         Long id,
         @NotBlank String name,
-        List<CardDTO> cards) {
+        List<CardDTO> cards,
+        UserDTO owner) {
 
     public static CardCollectionDTO fromEntity(CardCollection cardCollection) {
         List<CardDTO> cardDTOList = new ArrayList<CardDTO>();
         cardCollection.getCards().forEach(card -> {
             cardDTOList.add(CardDTO.fromEntity(card));
         });
-        return new CardCollectionDTO(cardCollection.getId(), cardCollection.getName(), cardDTOList);
+        return new CardCollectionDTO(cardCollection.getId(), cardCollection.getName(), cardDTOList,
+                UserDTO.fromEntity(cardCollection.getOwner()));
     }
 
     public static CardCollection toEntity(CardCollectionDTO dto) {
